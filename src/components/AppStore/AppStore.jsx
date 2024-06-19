5f990fee66e387ed8b794b3267ba5751dc7d2685
unknown <vihanidesaei543@gmail.com>
2024-06-19 16:49:47 +0530

    footer delete

--- src/components/AppStore/AppStore.jsx
+++ src/components/AppStore/AppStore.jsx
@@ -15,38 +15,6 @@ const bannerImg = {
 const AppStore = () => {
   return (
     <>
-      {/* <div
-        className="bg-gray-100 dark:bg-gray-800 text-white py-10"
-        style={bannerImg}
-      >
-        <div className="container">
-          <div
-            data-aos="fade-up"
-            data-aos-duration="300"
-            className="space-y-6 max-w-xl mx-auto"
-          >
-            <h1 className="text-2xl text-center sm:text-left sm:text-4xl font-semibold ">
-              Read Books at Your Fingertips
-            </h1>
-            <div className="flex flex-wrap justify-center items-center">
-              <a href="#">
-                <img
-                  src={PlayStoreImg}
-                  alt="Play store"
-                  className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
-                />
-              </a>
-              <a href="#">
-                <img
-                  src={AppStoreImg}
-                  alt="App store"
-                  className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]"
-                />
-              </a>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 };
